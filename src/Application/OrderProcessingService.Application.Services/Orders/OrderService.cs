19bec377417bc1cd548a3aa009dd9878386c0643
mibrgmv <mibrgmv@gmail.com>
2025-01-31 23:23:40 +0300

    feat: order service

--- src/Application/OrderProcessingService.Application.Services/Orders/OrderService.cs
+++ src/Application/OrderProcessingService.Application.Services/Orders/OrderService.cs
@@ -1,42 +1,227 @@
+using OrderProcessingService.Application.Abstractions.Persistence;
+using OrderProcessingService.Application.Abstractions.Persistence.Queries;
 using OrderProcessingService.Application.Contracts.Orders;
+using OrderProcessingService.Application.Contracts.Orders.Events;
 using OrderProcessingService.Application.Contracts.Orders.Operations;
+using OrderProcessingService.Application.Models;
+using OrderProcessingService.Infrastructure.Events;
+using System.Transactions;
 
 namespace OrderProcessingService.Application.Services.Orders;
 
 internal class OrderService : IOrderService
 {
-    public Task CreateAsync(CreateOrder.Request request, CancellationToken cancellationToken)
+    private readonly IPersistenceContext _context;
+    private readonly IEventPublisher _eventPublisher;
+
+    public OrderService(IPersistenceContext context, IEventPublisher eventPublisher)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _eventPublisher = eventPublisher;
     }
 
-    public Task<StartOrderProcessing.Result> StartProcessingAsync(StartOrderProcessing.Request request, CancellationToken cancellationToken)
+    public async Task CreateAsync(CreateOrder.Request request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var order = new Order(request.OrderId, OrderState.Created, request.CreatedAt, request.CreatedAt);
+        await _context.OrderRepository.AddOrUpdateAsync([order], cancellationToken);
     }
 
-    public Task<ApproveOrder.Result> ApproveAsync(ApproveOrder.Request request, CancellationToken cancellationToken)
+    public async Task<StartOrderProcessing.Result> StartProcessingAsync(
+        StartOrderProcessing.Request request,
+        CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var orderQuery = new OrderQuery([request.OrderId], 1, null);
+
+        Order? order = await _context.OrderRepository
+            .QueryAsync(orderQuery, cancellationToken)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (order is null)
+            return new StartOrderProcessing.Result.OrderNotFound();
+
+        order = order with
+        {
+            State = OrderState.PendingApproval,
+            UpdatedAt = request.StartedAt,
+        };
+
+        await _context.OrderRepository.AddOrUpdateAsync([order], cancellationToken);
+
+        return new StartOrderProcessing.Result.Success();
     }
 
-    public Task<StartOrderPacking.Result> StartPackingAsync(StartOrderPacking.Request request, CancellationToken cancellationToken)
+    public async Task<ApproveOrder.Result> ApproveAsync(ApproveOrder.Request request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var orderQuery = new OrderQuery([request.OrderId], 1, null);
+
+        Order? order = await _context.OrderRepository
+            .QueryAsync(orderQuery, cancellationToken)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (order is null)
+            return new ApproveOrder.Result.OrderNotFound();
+
+        if (order.State is not OrderState.PendingApproval)
+            return new ApproveOrder.Result.InvalidState(order.State);
+
+        order = order with
+        {
+            State = request.IsApproved ? OrderState.Approved : OrderState.Cancelled,
+            UpdatedAt = DateTimeOffset.UtcNow,
+        };
+
+        var evt = new OrderApprovalResultReceivedEvent(
+            request.OrderId,
+            request.IsApproved,
+            request.CreatedBy,
+            order.UpdatedAt);
+
+        using var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        await _context.OrderRepository.AddOrUpdateAsync([order], cancellationToken);
+        await _eventPublisher.PublishAsync(evt, cancellationToken);
+
+        transaction.Complete();
+
+        return new ApproveOrder.Result.Success();
     }
 
-    public Task<FinishOrderPacking.Result> FinishPackingAsync(FinishOrderPacking.Request request, CancellationToken cancellationToken)
+    public async Task<StartOrderPacking.Result> StartPackingAsync(StartOrderPacking.Request request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var orderQuery = new OrderQuery([request.OrderId], 1, null);
+
+        Order? order = await _context.OrderRepository
+            .QueryAsync(orderQuery, cancellationToken)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (order is null)
+            return new StartOrderPacking.Result.OrderNotFound();
+
+        if (order.State is not OrderState.Approved)
+            return new StartOrderPacking.Result.InvalidState(order.State);
+
+        order = order with
+        {
+            State = OrderState.Packing,
+            UpdatedAt = DateTimeOffset.UtcNow,
+        };
+
+        var evt = new OrderPackingStartedEvent(order.Id, request.PackingBy, order.UpdatedAt);
+
+        using var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        await _context.OrderRepository.AddOrUpdateAsync([order], cancellationToken);
+        await _eventPublisher.PublishAsync(evt, cancellationToken);
+
+        transaction.Complete();
+
+        return new StartOrderPacking.Result.Success();
     }
 
-    public Task<StartOrderDelivery.Result> StartDeliveryAsync(StartOrderDelivery.Request request, CancellationToken cancellationToken)
+    public async Task<FinishOrderPacking.Result> FinishPackingAsync(FinishOrderPacking.Request request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var orderQuery = new OrderQuery([request.OrderId], 1, null);
+
+        Order? order = await _context.OrderRepository
+            .QueryAsync(orderQuery, cancellationToken)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (order is null)
+            return new FinishOrderPacking.Result.OrderNotFound();
+
+        if (order.State is not OrderState.Packing)
+            return new FinishOrderPacking.Result.InvalidState(order.State);
+
+        order = order with
+        {
+            State = request.IsSuccessful ? OrderState.Packed : OrderState.Cancelled,
+            UpdatedAt = DateTimeOffset.UtcNow,
+        };
+
+        var evt = new OrderPackingFinishedEvent(
+            order.Id,
+            order.UpdatedAt,
+            request.IsSuccessful,
+            request.FailureReason);
+
+        using var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        await _context.OrderRepository.AddOrUpdateAsync([order], cancellationToken);
+        await _eventPublisher.PublishAsync(evt, cancellationToken);
+
+        transaction.Complete();
+
+        return new FinishOrderPacking.Result.Success();
     }
 
-    public Task<FinishOrderDelivery.Result> FinishDeliveryAsync(FinishOrderDelivery.Request request, CancellationToken cancellationToken)
+    public async Task<StartOrderDelivery.Result> StartDeliveryAsync(StartOrderDelivery.Request request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var orderQuery = new OrderQuery([request.OrderId], 1, null);
+
+        Order? order = await _context.OrderRepository
+            .QueryAsync(orderQuery, cancellationToken)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (order is null)
+            return new StartOrderDelivery.Result.OrderNotFound();
+
+        if (order.State is not OrderState.Packed)
+            return new StartOrderDelivery.Result.InvalidState(order.State);
+
+        order = order with
+        {
+            State = OrderState.InDelivery,
+            UpdatedAt = DateTimeOffset.UtcNow,
+        };
+
+        var evt = new OrderDeliveryStartedEvent(
+            order.Id,
+            request.DeliveredBy,
+            order.UpdatedAt);
+
+        using var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        await _context.OrderRepository.AddOrUpdateAsync([order], cancellationToken);
+        await _eventPublisher.PublishAsync(evt, cancellationToken);
+
+        transaction.Complete();
+
+        return new StartOrderDelivery.Result.Success();
+    }
+
+    public async Task<FinishOrderDelivery.Result> FinishDeliveryAsync(FinishOrderDelivery.Request request, CancellationToken cancellationToken)
+    {
+        var orderQuery = new OrderQuery([request.OrderId], 1, null);
+
+        Order? order = await _context.OrderRepository
+            .QueryAsync(orderQuery, cancellationToken)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (order is null)
+            return new FinishOrderDelivery.Result.OrderNotFound();
+
+        if (order.State is not OrderState.InDelivery)
+            return new FinishOrderDelivery.Result.InvalidState(order.State);
+
+        order = order with
+        {
+            State = request.IsSuccessful ? OrderState.Delivered : OrderState.Cancelled,
+            UpdatedAt = DateTimeOffset.UtcNow,
+        };
+
+        var evt = new OrderDeliveryFinishedEvent(
+            order.Id,
+            order.UpdatedAt,
+            request.IsSuccessful,
+            request.FailureReason);
+
+        using var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        await _context.OrderRepository.AddOrUpdateAsync([order], cancellationToken);
+        await _eventPublisher.PublishAsync(evt, cancellationToken);
+
+        transaction.Complete();
+
+        return new FinishOrderDelivery.Result.Success();
     }
 }
