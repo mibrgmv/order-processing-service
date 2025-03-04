6e3b84d0508a9d3fe796bd6f1d16115f6b445794
mibrgmv <mibrgmv@gmail.com>
2025-03-05 00:18:02 +0300

    refac: cursor

--- src/Application/OrderProcessingService.Application.Abstractions/Persistence/Queries/OrderQuery.cs
+++ src/Application/OrderProcessingService.Application.Abstractions/Persistence/Queries/OrderQuery.cs
@@ -2,5 +2,5 @@ namespace OrderProcessingService.Application.Abstractions.Persistence.Queries;
 
 public record OrderQuery(
     long[]? OrderIds,
-    long? Cursor,
+    long Cursor,
     int PageSize);
